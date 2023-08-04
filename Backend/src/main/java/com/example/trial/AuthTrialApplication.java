7168d0454418e061f898c424ef77d758ed951b11
Parvinderkaur Sodhi <65673880+Parvinderkaur-Sodhi@users.noreply.github.com>
2023-08-04 12:29:22 +0530

    Backend Setup

---
+++ Backend/src/main/java/com/example/trial/AuthTrialApplication.java
@@ -0,0 +1,13 @@
+package com.example.trial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthTrialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthTrialApplication.class, args);
+	}
+
+}
---
+++ Backend/src/main/java/com/example/trial/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.example.trial.repository;
+
+import com.example.trial.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository
+        extends JpaRepository<User, String> {
+
+    Optional<User> findByUsername(String username);
+    Boolean existsByUsername(String username);
+    Boolean existsByEmail(String email);
+
+}
